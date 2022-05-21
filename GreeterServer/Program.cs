609f36e6208f5a2ca11bebbd5d42eef09a7a5e55
WeiChunKao <34979078+WeiChunKao@users.noreply.github.com>
2022-05-21 20:41:52 +0800

    Add files via upload

---
+++ GreeterServer/Program.cs
@@ -0,0 +1,38 @@
+﻿using Greeter;
+using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreeterServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int port = 5555;
+            Server server = new Server
+            {
+                Services = { Greet.BindService(new GreetImpl()) },
+                Ports = { new ServerPort("localhost", port, ServerCredentials.Insecure) }
+            };
+            server.Start();
+
+            Console.WriteLine($"Greeter Server Listening on port {port}");
+            Console.WriteLine("Press Enter to exit");
+            Console.ReadLine();
+
+            server.ShutdownAsync().Wait();
+
+        }
+    }
+    class GreetImpl : Greet.GreetBase
+    {
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HelloReply { Message = $"Hello {request.Name}" });
+        }
+    }
+}
