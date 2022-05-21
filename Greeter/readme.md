4919d3dd5e8fe8e3bee4e8897c9d7e1538ef6310
WeiChunKao <34979078+WeiChunKao@users.noreply.github.com>
2022-05-21 20:40:32 +0800

    Add files via upload

---
+++ Greeter/readme.md
@@ -0,0 +1,4 @@
+﻿1. Add hello.proto and write content into hello.proto
+2. Open poject root folder and cmd
+3. Enter: packages\Grpc.Tools.2.45.0\tools\windows_x86\protoc.exe -I Greeter --csharp_out Greeter Greeter\hello.proto --grpc_out Greeter --plugin=protoc-gen-grpc=packages\Grpc.Tools.2.45.0\tools\windows_x86\grpc_csharp_plugin.exe
+Ref:https://tw511.com/a/01/15702.html
---
+++ GreeterClient/Program.cs
@@ -0,0 +1,24 @@
+﻿using Greeter;
+using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreeterClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Channel channel = new Channel("127.0.0.1:5555", ChannelCredentials.Insecure);
+            var client = new Greet.GreetClient(channel);
+            var replay = client.SayHello(new HelloRequest { Name = "Auto" });
+            Console.WriteLine($"{replay.Message}");
+
+            channel.ShutdownAsync().Wait();
+            Console.ReadLine();
+        }
+    }
+}
